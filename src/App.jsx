bce7e5a12836d264eecbdebe9fbc372ae384a07b
ImoEazy <imieadams@gmail.com>
2024-11-19 12:59:14 +0200

    added server files

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,30 @@
  * Review challenge: do all the CSS yourself based on the design
  * linked in the slides.
  */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
+import Login from "./pages/Login"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
+import NotFound from "./pages/NotFound"
+import Layout from "./components/Layout"
+import HostLayout from "./components/HostLayout"
+import AuthRequired from "./components/AuthRequired"
 
+import "./server"
 
+function App() {
   /**
    * Challenge: Create the AuthRequired Layout Route to protect
    * all the /host routes.
@@ -24,28 +46,45 @@
    * either send them to the /login route, or render the Outlet
    */
 
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route
+            path="login"
+            element={<Login />}
+          />
 
-  export default function App() {
+          <Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
+            </Route>
+          </Route>
 
-    return (
-      <BrowserRouter>
-        <header>
-          <Link to="/home">#VANLIFE</Link>
-          <nav>
-            <NavLink to="/about" className={({ isActive }) => isActive ? "active-link" : null}> About </NavLink>
-            <NavLink to="/vans" className={({ isActive }) => isActive ? "active-link" : null}> Vans For Hire </NavLink>
-          </nav>
-        </header>
-        <Routes> //container for a nested tree of route elements that renders the branch that best matshes the current location
-          <Route path="/home" element={<Home />} /> //declares an element that should be rendered at a certain RL path
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetails />} />
-        </Routes>
-      </BrowserRouter>
-    )
-  }
-  //BrowserRouter provides us with clean URLS, for use in search browsers
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
+
+
 
 
 
